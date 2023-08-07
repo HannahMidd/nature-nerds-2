ec325e58026bed4a2498ebd88590e815f87784b7
mavarreteno <mavarreteno@email.com>
2023-08-07 14:15:16 -0700

    added util templates

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,4 @@
-
+const gql = require('graphql-tag')
 
 const typeDefs = `
   type User {
@@ -82,7 +82,7 @@ const typeDefs = `
 
   type Mutation {
     login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String, password: String!): Auth
     savePark(parkData: ParkInput!): User
     removePark(parkId: ID!): User
   }
