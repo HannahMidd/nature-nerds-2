32cec9e963d420a9ba654519f9fdc839af596cfc
HannahMidd <hmiddleton1017@gmail.com>
2023-08-09 14:05:22 -0400

    fixing css, nav bar and photos

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -30,9 +30,9 @@
 //             onChange={handleSearchInputChange}
 //             placeholder="Park Input..."
 //           />
-          
+
 //           <button onClick={handleClearClick}>Clear</button>{" "}
-         
+
 //         </form>
 
 //         <ParkSearch searchTerm={searchInput} />
@@ -44,65 +44,59 @@
 
 // export default Search;
 
-
-
-
-
-import React, { useState } from "react";
-import AccuWeather from "./AccuWeather";
-import ParkSearch from "./ParkSearch";
-
+import React, { useState } from 'react';
+import AccuWeather from './AccuWeather';
+import ParkSearch from './ParkSearch';
 
 const Search = () => {
-  const [searchInput, setSearchInput] = useState("");
-  const [formSubmitted, setFormSubmitted] = useState(false);
+    const [searchInput, setSearchInput] = useState('');
+    const [formSubmitted, setFormSubmitted] = useState(false);
 
-  const handleSearchInputChange = (event) => {
-    setSearchInput(event.target.value);
-  };
+    const handleSearchInputChange = (event) => {
+        setSearchInput(event.target.value);
+    };
 
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
+    const handleSearchSubmit = (event) => {
+        event.preventDefault();
 
-  
+        setFormSubmitted(true);
+    };
 
-    setFormSubmitted(true);
-  };
+    const handleClearClick = () => {
+        setSearchInput('');
+        setFormSubmitted(false);
+    };
 
-  const handleClearClick = () => {
-    setSearchInput("");
-    setFormSubmitted(false);
-  };
+    return (
+        <section
+            id="parent-search-section"
+            className="parent-search parkSearchContainer"
+        >
+            <div className="parentComponentContainer parkSearchBox">
+                <h2 id="SearchTitle">Explore a National Park:</h2>
+                <form
+                    id="searchForm"
+                    onSubmit={handleSearchSubmit}
+                >
+                    <input
+                        type="text"
+                        value={searchInput}
+                        onChange={handleSearchInputChange}
+                        placeholder="Park Input..."
+                    />
+                    <button onClick={handleSearchSubmit}>Submit</button>
+                    <button onClick={handleClearClick}>Clear</button>{' '}
+                </form>
 
-  return (
-    <section id="parent-search-section" className="parent-search">
-      <div className="parentComponentContainer">
-        <form onSubmit={handleSearchSubmit}>
-          <input
-            type="text"
-            value={searchInput}
-            onChange={handleSearchInputChange}
-            placeholder="Park Input..."
-          />
-          <button onClick={handleSearchSubmit}>Submit</button>
-          <button onClick={handleClearClick}>Clear</button>{" "}
-        </form>
-
-        {formSubmitted && (
-          <>
-            <ParkSearch searchTerm={searchInput} />
-            <AccuWeather searchLocation={searchInput} />
-          </>
-        )}
-      </div>
-    </section>
-  );
+                {formSubmitted && (
+                    <>
+                        <ParkSearch searchTerm={searchInput} />
+                        <AccuWeather searchLocation={searchInput} />
+                    </>
+                )}
+            </div>
+        </section>
+    );
 };
 
 export default Search;
-
-
-
-
-
-
